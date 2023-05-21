52c55e9e7a61141f20069c27bbf97b39c0dbbe56
Zhuravlev <70717819+Zhuravlevvv@users.noreply.github.com>
2023-05-22 00:00:37 +0300

    Add files via upload

---
+++ OpTpAisd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpTpAisd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
