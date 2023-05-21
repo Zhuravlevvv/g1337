b271573648020a7f5ad15666139be5e73d94b961
Zhuravlev <70717819+Zhuravlevvv@users.noreply.github.com>
2023-05-21 23:57:05 +0300

    Add files via upload

---
+++ CountingSort/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace CountingSort
+{
+/*    Идея алгоритма заключается в следующем:
+
+    считаем количество вхождений каждого элемента массива;
+    исходя из данных полученных на первом шаге, формируем отсортированный массив.*/
+    class Program
+    {
+        
+        static int[] BasicCountingSort(int[] array, int k)
+        {
+            //поиск минимального и максимального значений
+            var min = array[0];
+            var max = array[0];
+            foreach (int element in array)
+            {
+                if (element > max)
+                {
+                    max = element;
+                }
+                else if (element < min)
+                {
+                    min = element;
+                }
+            }
+
+            //поправка
+            var correctionFactor = min != 0 ? -min : 0;
+            max += correctionFactor;
+
+            var count = new int[max + 1];
+            for (var i = 0; i < array.Length; i++)
+            {
+                count[array[i] + correctionFactor]++;
+            }
+
+            var index = 0;
+            for (var i = 0; i < count.Length; i++)
+            {
+                for (var j = 0; j < count[i]; j++)
+                {
+                    array[index] = i - correctionFactor;
+                    index++;
+                }
+            }
+
+            return array;
+        }
+
+        //метод для получения массива заполненного случайными числами
+        static int[] GetRandomArray(int arraySize, int minValue, int maxValue)
+        {
+            var random = new Random();
+            var randomArray = new int[arraySize];
+            for (var i = 0; i < randomArray.Length; i++)
+            {
+                randomArray[i] = random.Next(minValue, maxValue);
+            }
+
+            return randomArray;
+        }
+
+        static void Main(string[] args)
+        {
+            var arr = GetRandomArray(10, 0, 9);
+            Console.WriteLine("Входные данные: {0}", string.Join(", ", arr));
+            Console.WriteLine("Отсортированный массив: {0}", string.Join(", ", BasicCountingSort(arr, 9)));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GnomeSort/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace GnomeSort
+{
+    class Program
+    {
+        /*Алгоритм находит первую пару неотсортированных элементов массива
+        и меняет их местами.При этом учитывается факт, 
+        что обмент приводит к неправильному расположению 
+        элементов примыкающих с обеих сторон к только что переставленным.
+        Поскольку все элементы массива после переставленных не отсортированны, 
+        необходимо перепроверить только элементы до переставленных.*/
+        //метод для обмена элементов
+        static void Swap(ref int item1, ref int item2)
+        {
+            int temp = item1;
+            item1 = item2;
+            item2 = temp;
+        }
+
+        //Гномья сортировка
+        static int[] GnomeSort(int[] unsortedArray)
+        {
+            int index = 1;
+            int nextIndex = index + 1;
+
+            while (index < unsortedArray.Length)
+            {
+                if (unsortedArray[index - 1] < unsortedArray[index])
+                {
+                    index = nextIndex;
+                    nextIndex++;
+                }
+                else
+                {
+                    Swap(ref unsortedArray[index - 1], ref unsortedArray[index]);
+                    index--;
+                    if (index == 0)
+                    {
+                        index = nextIndex;
+                        nextIndex++;
+                    }
+                }
+            }
+
+            return unsortedArray;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Гномья сортировка");
+            Console.Write("Введите элементы массива: ");
+            string[] parts = Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+            int[] array = new int[parts.Length];
+            for (int i = 0; i < parts.Length; i++)
+            {
+                array[i] = Convert.ToInt32(parts[i]);
+            }
+
+            Console.WriteLine("Отсортированный массив: {0}", string.Join(", ", GnomeSort(array)));
+
+            Console.ReadLine();
+        }
+    }
+}
